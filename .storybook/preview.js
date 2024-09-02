ab36b90775b13dcb4e38e90145763b3708a7bd62
Andrii Rybak <andrey.r88816@gmail.com>
2024-09-02 19:49:26 +0300

    first commit

---
+++ .storybook/preview.js
@@ -0,0 +1,16 @@
+import "../src/index.css";
+
+//👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
